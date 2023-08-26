c7ff5fff6c19e2cea1d6a35918aab38e6376410d
Psagrox <waljaviergalvan@gmail.com>
2023-08-25 23:18:41 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const colorPicker = document.getElementByID ("colorPicker");
 
 
-colorPicker.addEventListener("input", function (event){
-    const selectedColor = event.target.value;
+colorPicker.addEventListener("input", function (e){
+    const selectedColor = e.target.value;
     //Use the variable selectedColor to register the option that the user choose.
 });
 
 const rgb = document.getElementById("RGB");
 
-rgb.addEventListener("click", function(){
+rgb.addEventListener("click", function() {
     function generateNumber(number){
-        return (Math.random()*number).toFixed(0);
+        return Math.floor(Math.random()*number);
     };
 
     function rgbColor(){
-        var colorRgb = "(" + generateNumber(255) + "," + generateNumber(255) + "," + generateNumber(255) + ")";
-        return "rgb" + colorRgb;
+        var colorRgb = "rgb(" + 
+        generateNumber(255) + 
+        "," + 
+        generateNumber(255) + 
+        "," + 
+        generateNumber(255) + 
+        ")";
+        return colorRgb;
     }
+
+    const radomColor = rgbColor();
+    //use the random RGB color generated to update de streak of my canvas.
 });
+
